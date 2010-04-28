f5d80ca8f23c220f06fe3feafe1bf63fd48ca90e
reilly@reilly-mbpro <reilly@reilly-mbpro>
2010-04-27 21:48:54 -0400

    Quote app now a game instead of just a quote loader.  Totally works and is amazing, ship it!

--- simpsonsquote/src/com/vasken/SimpsonsQuote/Main.java
+++ simpsonsquote/src/com/vasken/SimpsonsQuote/Main.java
@@ -1,39 +1,109 @@
 package com.vasken.SimpsonsQuote;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Main extends Activity {
 	QuoteStore quotestore;
-    /** Called when the activity is first created. */
+	int questionNumber;
+	Button opt1;
+	Button opt2;
+	Button opt3;
+	String currentEpisode;
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        opt1 = (Button)findViewById(R.id.opt1);
+        opt2 = (Button)findViewById(R.id.opt2);
+        opt3 = (Button)findViewById(R.id.opt3);
         try {
 			quotestore = new QuoteStore(this);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		questionNumber = 0;
 		loadNewQuote();
-		((WebView)findViewById(R.id.quote)).setOnTouchListener(new OnTouchListener() {
-			public boolean onTouch(View v, MotionEvent event) {
-				if (event.getAction() == MotionEvent.ACTION_UP) {
-					loadNewQuote();
-				}
-				return true;
-			}});
+		opt1.setOnClickListener(buttonClicked);
+		opt2.setOnClickListener(buttonClicked);
+		opt3.setOnClickListener(buttonClicked);
+    }
+    
+    OnClickListener buttonClicked = new OnClickListener() {
+		public void onClick(View v) {
+			Button b = (Button)v;
+			String status;
+			if (b.getText().equals(currentEpisode)) {
+				status = "Correct, you are clearly amazing!";
+			}
+			else {
+				status = "Wrong, dummy!  That was from " + currentEpisode;
+			}
+			Log.d("WHERE'S MY TOAST!", status);
+			Toast.makeText(Main.this, status, Toast.LENGTH_SHORT).show();
+			loadNewQuote();
+		}};
+    
+    // Fischer-Yeates shuffle
+    static Random rng = new Random();
+    <T extends Object>void shuffle(List<T> list) {
+        // n is the number of items remaining to be shuffled.
+        for (int n = list.size(); n > 1; n--) {
+            // Pick a random element to swap with the nth element.
+            int k = rng.nextInt(n);  // 0 <= k <= n-1 (0-based array)
+            // Swap array elements.
+            T tmp = list.get(k);
+            list.set(k, list.get(n - 1));
+            list.set(n - 1, tmp);
+        }
+    }
+    
+    <T extends Object>void populateListWithUniqueElements(List<T> list, int desiredSize, Callable<T> generator) {
+    	try {
+	    	while (list.size() < desiredSize) {	
+	    		T newObj = generator.call();
+	    		if (!list.contains(newObj)) {
+	    			list.add(newObj);
+	    		}
+	    	}
+    	} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
     void loadNewQuote() {
+    	questionNumber++;
+    	if (questionNumber > 20)
+    		questionNumber = 1;
     	WebView quoteview = (WebView)findViewById(R.id.quote);
-		SimpsonsQuote quote = quotestore.randomQuoteThatIsntTerrible();
+    	SimpsonsQuote quote = quotestore.quoteBySeason(questionNumber);
+		List<String> episodes = new ArrayList<String>();
+		currentEpisode = quote.episode;
+		episodes.add(quote.episode);
+		populateListWithUniqueElements(episodes, 3, new Callable<String>() { public String call() throws Exception {
+				return quotestore.randomEpisode();
+			}});
+		
+		shuffle(episodes);
     	quoteview.loadData(quote.quote, "text/html", "utf-8");
+    	opt1.setText(episodes.get(0));
+    	opt2.setText(episodes.get(1));
+    	opt3.setText(episodes.get(2));
     }
 }
--- simpsonsquote/src/com/vasken/SimpsonsQuote/QuoteStore.java
+++ simpsonsquote/src/com/vasken/SimpsonsQuote/QuoteStore.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
@@ -13,25 +14,43 @@ import android.util.Log;
 
 public class QuoteStore {
 	private List<SimpsonsQuote> quotes = new ArrayList<SimpsonsQuote>();
+	private HashMap<String, List<SimpsonsQuote>> quotesBySeason = new HashMap<String, List<SimpsonsQuote>>();
 	private Random rand = new Random();
 	
 	public QuoteStore(Context context) throws IOException {
-		InputStream in = context.getResources().openRawResource(R.raw.quotes);
+		for (int i = 1; i <= 21; i++) {
+			quotesBySeason.put("Season " + Integer.toString(i), new ArrayList<SimpsonsQuote>());
+		}
+		InputStream in = context.getResources().openRawResource(R.raw.arrested_development);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 		String season = reader.readLine();
 		while (season != null) {
 			String epTitle = reader.readLine();
 			String quote = reader.readLine();
 			if (season != null && epTitle != null && quote != null) {
-				quotes.add(new SimpsonsQuote(season, epTitle, quote));
+				SimpsonsQuote sq = new SimpsonsQuote(season, epTitle, quote);
+				quotes.add(sq);
+				quotesBySeason.get(season).add(sq);
 			}
 			season = reader.readLine();
 		}
 		Log.d(getClass().getName(), "Found " + Integer.toString(quotes.size()) + " quotes");
 	}
 	
+	public SimpsonsQuote randomQuoteFromList(List<SimpsonsQuote> list) {
+		return list.get(rand.nextInt(list.size()));
+	}
+	
 	public SimpsonsQuote randomQuote() {
-		return quotes.get(rand.nextInt(quotes.size()));
+		return randomQuoteFromList(quotes);
+	}
+	
+	public SimpsonsQuote quoteBySeason(int season) {
+		return randomQuoteFromList(quotesBySeason.get("Season " + Integer.toString(season)));
+	}
+	
+	public String randomEpisode() {
+		return randomQuote().episode;
 	}
 	
 	// performance on this isn't so great...
