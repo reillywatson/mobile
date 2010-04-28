07b0258aad1eb8e2ae2c7d1c87bdf7f31cad4b06
reilly@reilly-mbpro <reilly@reilly-mbpro>
2010-04-27 21:47:43 -0400

    New, better parser.  Parses more than just Simpsons -- tested with Arrested Development

--- incubator/simparse.py
+++ incubator/simparse.py
@@ -1,39 +1,44 @@
 import BeautifulSoup
 import urllib2
+import os
 
 def main():
-	quotes = open("/Users/reilly/quotes.txt", "w")
-	infile = urllib2.urlopen("file:///Users/reilly/hgrepo/incubator/simpsonswikiquote.htm")
-	soup = BeautifulSoup.BeautifulSoup(infile)
+	quotes = open("quotesout.txt", "w")
+	os.system("wget http://en.wikiquote.org/wiki/Arrested_Development --output-document=wgetoutput")
+	infile = open("wgetoutput", "r")
+	instr = infile.read();
+	infile.close();
+	os.remove("wgetoutput")
+	soup = BeautifulSoup.BeautifulSoup(instr)
 	seasonName = ""
 	eptitle = ""
-	for tag in soup.body.contents:
+	contents = soup.find(id="bodyContent").contents
+	for tag in contents:
 		if not 'name' in dir(tag):
 			pass
-		elif tag.name == "h2":
-			seasonName = tag.contents[2].contents[0].contents[0]
-			print seasonName
-			if 'text' in seasonName:
-				seasonName = seasonName.text
-			seasonName = seasonName + '\n'
-		elif tag.name == "h3":
-			eptitle = tag.contents[2].contents[0].contents[0]
-			if 'title' in eptitle:
-				eptitle = eptitle['title']
-			if 'text' in dir(eptitle):
-				eptitle = eptitle.text
-			print eptitle
-			eptitle = eptitle + '\n'
-		elif tag.name == "dl":
-			for sub in tag.findAll('a'):
-				sub.replaceWith(sub.text)
-			epquote = tag.prettify().replace("\n", "")
-			try:
-				quotes.write(seasonName + eptitle + epquote + '\n')
-			except UnicodeDecodeError:
-				pass
-			except UnicodeEncodeError:
-				pass
+		else:
+			if tag.name == "h2":
+				seasonName = tag.text.replace("[edit]", "")
+				print seasonName
+				if seasonName.lower().find('season') > -1:
+					seasonName = seasonName + '\n'
+				else:
+					seasonName=""
+			if seasonName != "":
+				if tag.name == "h3":
+					eptitle = tag.text.replace("[edit]", "")
+					print eptitle
+					eptitle = eptitle + '\n'
+				elif tag.name == "dl":
+					for sub in tag.findAll('a'):
+						sub.replaceWith(sub.text)
+					epquote = tag.prettify().replace("\n", "")
+					try:
+						quotes.write(seasonName + eptitle + epquote + '\n')
+					except UnicodeDecodeError:
+						pass
+					except UnicodeEncodeError:
+						pass
 
 
 if __name__ == '__main__':
