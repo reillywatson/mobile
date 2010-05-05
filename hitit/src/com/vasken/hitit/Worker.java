001c2919b41779d70eb6286b8c3d4332c587b055
Reilly Watson <reillywatson@gmail.com>
2010-05-05 03:17:32 -0400

    Pull out some code that will probably be useful elsewhere

--- hitit/src/com/vasken/hitit/Worker.java
+++ hitit/src/com/vasken/hitit/Worker.java
@@ -1,45 +1,38 @@
 package com.vasken.hitit;
 
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 
-import android.content.Context;
+import com.vasken.util.WebRequester;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 
-public class Worker {
-	
-	private DefaultHttpClient httpclient;
-	
+public class Worker implements WebRequester.RequestCallback {	
 	private HttpPost httpPost;
-    
-	private HttpResponse response;
-	
-	private final char[] buffer = new char[0x10000];
-	private final StringBuilder sb = new StringBuilder(64*1024);
 
     private Pattern p =  Pattern.compile("<img id='mainPic'.*?src='(.*?)'.*?>.*?<input type=\"hidden\" name=\"ratee\" value=\"(.*?)\".*?>", Pattern.DOTALL);
     private Pattern prevRatingRegex = Pattern.compile("class=\"score\".*?>(.*?)</div>.*?Based on (.*?) votes", Pattern.DOTALL);
-
-	public Worker(Context context) {
-		httpPost = new HttpPost(context.getString(R.string.rate_url_female));
-        httpclient = new DefaultHttpClient();
-		
+    private WebRequester requester = new WebRequester();
+    
+    String id;
+    int rating;
+    HotItem item;
+    
+	public Worker(String postUrl) {
+		httpPost = new HttpPost(postUrl);		
         httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
 		
 	}
@@ -50,68 +43,28 @@ public class Worker {
 	}
 	
 	private HotItem requestDataFromServer(String id, int rating) {
-		sb.delete(0, sb.length());
-		try {
-			Log.d(getClass().getName(), "<<<<<<<<<< Start Page Loading" + rating +"    " + id);
-
-			if (rating > 0 && id != null) {
-				List<NameValuePair> params = new ArrayList<NameValuePair>();
-		        params.add(new BasicNameValuePair("rate", "1"));
-		        params.add(new BasicNameValuePair("state", "rate"));
-		        params.add(new BasicNameValuePair("minR", "9"));
-		        params.add(new BasicNameValuePair("rateAction", "vote"));
-		        params.add(new BasicNameValuePair("ratee", id));
-		        params.add(new BasicNameValuePair("r"+id, ""+rating));
-		        httpPost.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
-				
-				Log.d(getClass().getName(), "Rating Set...  " + id + ": " + rating);
+		Log.d(getClass().getName(), "<<<<<<<<<< Start Page Loading" + rating +"    " + id);
+		this.id = id;
+		this.rating = rating;
+		item = null;
+		if (rating > 0 && id != null) {
+			List<NameValuePair> params = new ArrayList<NameValuePair>();
+	        params.add(new BasicNameValuePair("rate", "1"));
+	        params.add(new BasicNameValuePair("state", "rate"));
+	        params.add(new BasicNameValuePair("minR", "9"));
+	        params.add(new BasicNameValuePair("rateAction", "vote"));
+	        params.add(new BasicNameValuePair("ratee", id));
+	        params.add(new BasicNameValuePair("r"+id, ""+rating));
+	        try {
+				httpPost.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
+			} catch (UnsupportedEncodingException e) {
+				return null;
 			}
-			response = httpclient.execute(httpPost);
-            Reader in = new InputStreamReader(response.getEntity().getContent());
-
-	        int bytesRead = in.read(buffer, 0, buffer.length);
-	        int totalBytesRead = 0;
-	        while (bytesRead>=0) {
-	        	sb.append(buffer, 0, bytesRead);
-	        	totalBytesRead += bytesRead;
-	        	bytesRead = in.read(buffer, 0, buffer.length);
-	        	// This seems like the sort of thing that could no longer be true in the future.
-	        	// I suppose worst case, the app would just run a bit slower.
-	        	if (totalBytesRead > 26000) {
-		            Matcher m = p.matcher(sb);
-		            
-		            boolean matches = m.find();
-		            
-		            if(matches) {
-		            	try {
-		            		HotItem theHotItem = new HotItem();
-		            		if (id != null && rating > 0) {
-		            			addRatingResults(id, sb, theHotItem);
-		            		}
-		            		
-		                	URL url = new URL(m.group(1));
-		        	    	InputStream is = (InputStream)url.getContent();
-		        	    	
-		        	    	// This seemed promissing, but it's incredibly slow
-//		        	    	BitmapFactory.Options options = new BitmapFactory.Options();
-//		        	    	options.inSampleSize = 2;		        	    	
-		        	    	Bitmap image = BitmapFactory.decodeStream(is, null, null);
-		        			
-		        			theHotItem.setImage(image);
-		        			theHotItem.setRateId( m.group(2));
-		                	in.close();
-		        	        Log.d(getClass().getName(), ">>>>>>>>>>> Done Page Loading");
-		                	return theHotItem;
-						} catch (Exception e) { Log.d(this.getClass().toString(), "Not a valid imageUrl: " + m.group(1));	}
-		            }
-	        	}
-	        }
-		} catch (Exception e) {
-			Log.d(this.getClass().getName(), Log.getStackTraceString(e));
+			
+			Log.d(getClass().getName(), "Rating Set...  " + id + ": " + rating);
 		}
-
-		Log.d(getClass().getName(), ">>>>>>>>>>> Done Page Loading");
-		return null;
+		requester.makeRequest(httpPost, this);
+		return item;
 	}
 	
 	void addRatingResults(String id, StringBuilder sb, HotItem item) {
@@ -127,5 +80,37 @@ public class Worker {
 			Log.d(getClass().getName(), "Average Rating: " + avgRating);
 			Log.d(getClass().getName(), "Num ratings: " + numRatings);
 		}
+	}
+
+	@Override
+	public boolean handlePartialResponse(StringBuilder sb) {
+		// This (26000 bytes) seems like the sort of thing that could no longer be true in the future.
+    	// I suppose worst case, the app would just run a bit slower.
+    	if (sb.length() > 26000) {
+            Matcher m = p.matcher(sb);
+            
+            boolean matches = m.find();
+            
+            if(matches) {
+            	try {
+            		HotItem theHotItem = new HotItem();
+            		if (id != null && rating > 0) {
+            			addRatingResults(id, sb, theHotItem);
+            		}
+            		
+                	URL url = new URL(m.group(1));
+        	    	InputStream is = (InputStream)url.getContent();
+        	    	
+        	    	Bitmap image = BitmapFactory.decodeStream(is, null, null);
+        			
+        			theHotItem.setImage(image);
+        			theHotItem.setRateId( m.group(2));
+        	        Log.d(getClass().getName(), ">>>>>>>>>>> Done Page Loading");
+        	        item = theHotItem;
+                	return true;
+				} catch (Exception e) { Log.d(this.getClass().toString(), "Not a valid imageUrl: " + m.group(1));	}
+            }
+    	}
+    	return false;
 	}
 }
---
+++ hitit/src/com/vasken/util/WebRequester.java
@@ -0,0 +1,53 @@
+package com.vasken.util;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import android.util.Log;
+
+public class WebRequester {
+	
+	public interface RequestCallback {
+		// return true when you've gotten back enough of the response
+		boolean handlePartialResponse(StringBuilder responseSoFar);
+	}
+	
+	private DefaultHttpClient httpclient = new DefaultHttpClient();
+	private final char[] buffer = new char[0x10000];
+	private final StringBuilder sb = new StringBuilder(64*1024);
+
+	public void makeRequest(HttpUriRequest request, RequestCallback callback) {
+		Reader in = null;
+		try {
+			sb.delete(0, sb.length());
+			HttpResponse response = httpclient.execute(request);
+	        in = new InputStreamReader(response.getEntity().getContent());
+	
+	        int bytesRead = in.read(buffer, 0, buffer.length);
+	        int totalBytesRead = 0;
+	        while (bytesRead>=0) {
+	        	sb.append(buffer, 0, bytesRead);
+	        	totalBytesRead += bytesRead;
+	        	bytesRead = in.read(buffer, 0, buffer.length);
+	        	
+	        	if (callback.handlePartialResponse(sb))
+	        		break;
+	        }
+		} catch (Exception e) {
+			Log.d(this.getClass().getName(), Log.getStackTraceString(e));
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					// We're hosed, just leave the damn thing open.
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
