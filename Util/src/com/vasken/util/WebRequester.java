6d5418e8c189d00e39cf072120fe926c993a6462
Reilly Watson <reillywatson@gmail.com>
2010-05-06 07:59:13 -0400

    Remove a function I'm not currently using, because I don't know if it works

--- Util/src/com/vasken/util/WebRequester.java
+++ Util/src/com/vasken/util/WebRequester.java
@@ -23,20 +23,6 @@ import android.util.Log;
 
 public class WebRequester {
 	
-	public static byte[] bytesFromUrl(String urlString) throws IOException {
-		URL url = new URL(urlString);
-		InputStream in = new BufferedInputStream(url.openStream());
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		byte[] buf = new byte[1024];
-		int n = 0;
-		while (-1!=(n=in.read(buf))) {
-			out.write(buf, 0, n);
-		}
-		out.close();
-		in.close();
-		return out.toByteArray();
-	}
-	
 	public static Bitmap bitmapFromUrl(String urlString) throws IOException {
 		URL bitmapUrl = new URL(urlString);
         HttpGet httpRequest = null;
