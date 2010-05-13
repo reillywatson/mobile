ee79c4afd04feba68db87c9d1a2f1a7b7bf210f8
Reilly Watson <reillywatson@gmail.com>
2010-05-13 00:18:00 -0400

    Warnings.

--- Comics/src/com/vasken/comics/Downloaders/XKCDDownloader.java
+++ Comics/src/com/vasken/comics/Downloaders/XKCDDownloader.java
@@ -1,9 +1,6 @@
 package com.vasken.comics.Downloaders;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import android.util.Log;
 
 public class XKCDDownloader extends Downloader {
 	
