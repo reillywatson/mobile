d89fac15f6a3ea89e4fa231a686f239f224a39a1
Reilly Watson <reillywatson@gmail.com>
2010-05-13 23:41:49 -0400

    Some code to move favourites to the front of the list

--- Comics/src/com/vasken/comics/Main.java
+++ Comics/src/com/vasken/comics/Main.java
@@ -1,6 +1,9 @@
 package com.vasken.comics;
 
+import java.util.ArrayList;
+
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,11 +16,34 @@ public class Main extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-    	Object[] comics = ComicList.comicList().toArray();
-    	ListAdapter adapter = new ArrayAdapter<Object>(this, android.R.layout.simple_list_item_1, comics);
+    	ArrayList<ComicInfo> comics = ComicList.comicList();
+    	moveFavoritesToFront(comics);
+    	ListAdapter adapter = new ArrayAdapter<Object>(this, android.R.layout.simple_list_item_1, comics.toArray());
     	setListAdapter(adapter);
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    // TODO: we need to put these in a different section, which I suppose means making a ListAdapter class rather than
+    // just using ArrayAdapter
+    private void moveFavoritesToFront(ArrayList<ComicInfo> comics) {
+    	String favoritesList = getPreferences(Context.MODE_PRIVATE).getString("favorites", null);
+    	if (favoritesList != null) {
+    		String[] favorites = favoritesList.split(",");
+    		for(String favorite : favorites) {
+    			ComicInfo match = null;
+    			for (ComicInfo info : comics) {
+    				if (info.name.equals(favorite)) {
+    					match = info;
+    					break;
+    				}
+    			}
+    			if (match != null) {
+    				comics.remove(match);
+    				comics.add(0, match);
+    			}
+    		}
+    	}
     }
     
     protected void onListItemClick(ListView l, View v, int position, long id) {
