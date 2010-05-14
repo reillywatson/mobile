f1f05affac396f3d20fb89030f77a44a3719a45c
Reilly Watson <reillywatson@gmail.com>
2010-05-13 23:22:08 -0400

    Handle rotation

--- Comics/src/com/vasken/comics/Viewer.java
+++ Comics/src/com/vasken/comics/Viewer.java
@@ -15,7 +15,8 @@ import com.vasken.util.UserTask;
 
 public class Viewer extends Activity {
 	Downloader currentDownloader;
-	ComicInfo currentComic;
+	ComicInfo currentComicInfo;
+	Comic currentComic;
 	String currentURL;
 	boolean successfullyLoadedFirstComic;
 
@@ -23,6 +24,18 @@ public class Viewer extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.viewer);
+		Viewer rotatedSelf = (Viewer)getLastNonConfigurationInstance();
+		if (rotatedSelf != null) {
+			currentDownloader = rotatedSelf.currentDownloader;
+			currentComicInfo = rotatedSelf.currentComicInfo;
+			currentComic = rotatedSelf.currentComic;
+			currentURL = rotatedSelf.currentURL;
+			successfullyLoadedFirstComic = rotatedSelf.successfullyLoadedFirstComic;
+			if (currentComic != null) {
+				loadComic(currentComic);
+			}
+			return;
+		}
 		int comicNo = 0;
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
@@ -34,13 +47,18 @@ public class Viewer extends Activity {
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		if (currentComic != null && currentURL != null) {
-			getPreferences(Context.MODE_PRIVATE).edit().putString("lastRead" + currentComic.name, currentURL).commit();
+		if (currentComicInfo != null && currentURL != null) {
+			getPreferences(Context.MODE_PRIVATE).edit().putString("lastRead" + currentComicInfo.name, currentURL).commit();
 		}
+	}
+	
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		return this;
 	}
 
 	public void selectComic(ComicInfo info) {
-		currentComic = info;
+		currentComicInfo = info;
 		try {
 			currentDownloader = info.downloaderConstructor.call();
 			currentDownloader.setDefaultUrl(info.startUrl);
@@ -56,6 +74,60 @@ public class Viewer extends Activity {
 		currentDownloader.setUrl(url);
 		new DownloadTask().execute(currentDownloader);
 	}
+	
+	void loadComic(final Comic comic) {
+		if (comic != null) {
+			currentComic = comic;
+			if (comic.image == null) {
+				if (!successfullyLoadedFirstComic && !currentDownloader.getUrl().equals(currentComicInfo.startUrl)) {
+					downloadComic(currentComicInfo.startUrl);
+					return;
+				}
+			}
+			TextView alt = (TextView) Viewer.this.findViewById(R.id.alt_text);
+			WebView webView = (WebView) Viewer.this.findViewById(R.id.WebView);
+			webView.setVisibility((comic.image != null) ? View.VISIBLE : View.GONE);
+			alt.setVisibility((comic.altText != null) ? View.VISIBLE : View.GONE);
+			if (comic.altText != null) {
+				alt.setText(StringUtils.unescapeHtml(comic.altText));
+			}
+			if (comic.image != null) {
+				webView.loadUrl(comic.image);
+				currentURL = comic.permalink;
+				successfullyLoadedFirstComic = true;
+			} else {
+				currentURL = null;
+				alt.setVisibility(View.VISIBLE);
+				alt.setText("Unable to load comic");
+			}
+			Button prev = (Button) Viewer.this.findViewById(R.id.prev_comic);
+			Button next = (Button) Viewer.this.findViewById(R.id.next_comic);
+			boolean enablePrev = comic.prevUrl != null && !comic.prevUrl.equals(comic.url);
+			boolean enableNext = comic.nextUrl != null && !comic.nextUrl.equals(comic.url);
+			prev.setEnabled(enablePrev);
+			next.setEnabled(enableNext);
+			if (enablePrev) {
+				prev.setOnClickListener(new OnClickListener() {
+					public void onClick(View arg0) {
+						Viewer.this.downloadComic(comic.prevUrl);
+					}
+				});
+			}
+			if (enableNext) {
+				next.setOnClickListener(new OnClickListener() {
+					public void onClick(View arg0) {
+						Viewer.this.downloadComic(comic.nextUrl);
+					}
+				});
+			}
+			TextView tv = (TextView) Viewer.this.findViewById(R.id.title);
+			if (comic.title != null) {
+				tv.setText(StringUtils.unescapeHtml(comic.title));
+			} else {
+				tv.setText("");
+			}
+		}
+	}
 
 	class DownloadTask extends UserTask<Downloader, Void, Comic> {
 		@Override
@@ -65,57 +137,7 @@ public class Viewer extends Activity {
 
 		@Override
 		public void onPostExecute(final Comic comic) {
-			if (comic != null) {
-				if (comic.image == null) {
-					if (!successfullyLoadedFirstComic && !currentDownloader.getUrl().equals(currentComic.startUrl)) {
-						downloadComic(currentComic.startUrl);
-						return;
-					}
-				}
-				TextView alt = (TextView) Viewer.this.findViewById(R.id.alt_text);
-				WebView webView = (WebView) Viewer.this.findViewById(R.id.WebView);
-				webView.setVisibility((comic.image != null) ? View.VISIBLE : View.GONE);
-				alt.setVisibility((comic.altText != null) ? View.VISIBLE : View.GONE);
-				if (comic.altText != null) {
-					alt.setText(StringUtils.unescapeHtml(comic.altText));
-				}
-				if (comic.image != null) {
-					webView.loadUrl(comic.image);
-					currentURL = comic.permalink;
-					successfullyLoadedFirstComic = true;
-				} else {
-					currentURL = null;
-					alt.setVisibility(View.VISIBLE);
-					alt.setText("Unable to load comic");
-				}
-				Button prev = (Button) Viewer.this.findViewById(R.id.prev_comic);
-				Button next = (Button) Viewer.this.findViewById(R.id.next_comic);
-				boolean enablePrev = comic.prevUrl != null && !comic.prevUrl.equals(comic.url);
-				boolean enableNext = comic.nextUrl != null && !comic.nextUrl.equals(comic.url);
-				prev.setEnabled(enablePrev);
-				next.setEnabled(enableNext);
-				if (enablePrev) {
-					prev.setOnClickListener(new OnClickListener() {
-						public void onClick(View arg0) {
-							Viewer.this.downloadComic(comic.prevUrl);
-						}
-					});
-				}
-				if (enableNext) {
-					next.setOnClickListener(new OnClickListener() {
-						public void onClick(View arg0) {
-							Viewer.this.downloadComic(comic.nextUrl);
-						}
-					});
-				}
-				TextView tv = (TextView) Viewer.this.findViewById(R.id.title);
-				if (comic.title != null) {
-					tv.setText(StringUtils.unescapeHtml(comic.title));
-				} else {
-					tv.setText("");
-				}
-
-			}
+			loadComic(comic);
 		}
 	}
 
