ba7bbf5af756656cef6c6fc5e38f53abd0d147e5
Reilly Watson <reillywatson@gmail.com>
2010-05-25 01:48:13 -0400

    Updated HunkOrJunk to use our webserver, and to show results.

---
+++ HunkOrJunk/src/com/vasken/hunkorjunk/AppEngineWorker.java
@@ -0,0 +1,82 @@
+package com.vasken.hunkorjunk;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.http.client.methods.HttpGet;
+
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import com.vasken.util.WebRequester;
+
+public class AppEngineWorker extends Worker {
+	private HttpGet httpGet;
+	
+	private static Queue<HotItem> items = new LinkedList<HotItem>();
+	
+	private Pattern hotItemPattern =  Pattern.compile(
+		"<HotItem>" +
+			"<RatingID>(.*?)</RatingID>" +
+			"<ImageURL>(.*?)</ImageURL>" +
+			"<AverageRating>(.*?)</AverageRating>" +
+			"<NumRatings>(.*?)</NumRatings>" +
+		"</HotItem>"
+		, Pattern.DOTALL);
+			
+	public AppEngineWorker(String url) {
+		httpGet = new HttpGet(url + "?NumResults=10");
+	}
+
+	@Override
+	public boolean handlePartialResponse(StringBuilder dataSoFar, boolean isFinal) {
+		if (isFinal) {
+			Matcher m = hotItemPattern.matcher(dataSoFar);
+			while (m.find()) {
+				try {
+					HotItem theHotItem = new HotItem();
+
+	    			theHotItem.setRateId(m.group(1));
+					theHotItem.setImageURL(m.group(2));
+					theHotItem.setResultAverage(Double.parseDouble(m.group(3)));
+					theHotItem.setResultTotals(m.group(4));
+	        		
+					Bitmap image = WebRequester.bitmapFromUrl(theHotItem.getImageURL());
+	    			theHotItem.setImage(image);
+	            	
+	    			items.add(theHotItem);
+				} catch (IOException e) {
+					Log.d(getClass().getName(), Log.getStackTraceString(e));
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public HotItem getPageData(String rateId, int rating, String imageURL) {
+		long startTime = System.currentTimeMillis();
+		HotItem theCurrentItem = items.poll();
+		boolean needToAskServer = theCurrentItem == null;
+		if (needToAskServer) {
+			// Magically populate 'items'
+			new WebRequester().makeRequest(httpGet, this);
+			
+			// Grab the first item
+			theCurrentItem = items.poll();
+		}
+		
+		if (theCurrentItem == null) {
+			Log.d(getClass().getName(), "OH GOD! We're in BIG trouble - theCurrentItem is null.");
+		}
+		
+		Log.d(getClass().getName(), 
+				"Took " + (System.currentTimeMillis() - startTime) + " from the " + (needToAskServer? " SERVER " : " LOCAL QUEUE"));
+		return theCurrentItem;
+	}
+
+}
