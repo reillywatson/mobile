2f58bef66db8d1c335027bbc99c0285f6263ac5b
Reilly Watson <reillywatson@gmail.com>
2010-07-23 13:45:18 -0400

    Kind-of working version of name that tune.  Awesome!

--- namethattune/src/com/vasken/namethattune/Main.java
+++ namethattune/src/com/vasken/namethattune/Main.java
@@ -1,56 +1,131 @@
 package com.vasken.namethattune;
 
-import com.vasken.namethattune.PreviewURLFactory.PreviewURLListener;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.vasken.namethattune.PreviewURLProvider.PreviewURLListener;
+import com.vasken.util.UserTask;
+import net.roarsoftware.lastfm.Track;
 
 import android.app.Activity;
-import android.media.AsyncPlayer;
-import android.media.AudioManager;
-import android.net.Uri;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
-import android.widget.TextView.OnEditorActionListener;
 
-public class Main extends Activity implements PreviewURLListener {
+public class Main extends Activity {
     /** Called when the activity is first created. */
 	
-	private boolean isDownloading = false;
-	private AsyncPlayer player = new AsyncPlayer("previewer");
+	private MediaPlayer player = new MediaPlayer();
+	private Button opt1, opt2, opt3, opt4;
+	String correct;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        EditText edit = (EditText)findViewById(R.id.EditText01);
-        edit.setOnEditorActionListener(new OnEditorActionListener() {
-			@Override
-			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-				if (isDownloading)
-					return false;
-				if (event.getAction() == KeyEvent.ACTION_UP) {
-					Log.d("download started", v.getText().toString());
-					isDownloading = true;
-					new PreviewURLFactory().getPreviewURL(v.getText().toString(), Main.this);
-				}
-				return true;
-			}
-        	
-        });
+        opt1 = (Button)findViewById(R.id.Button01);
+        opt2 = (Button)findViewById(R.id.Button02);
+        opt3 = (Button)findViewById(R.id.Button03);
+        opt4 = (Button)findViewById(R.id.Button04);
+        opt1.setOnClickListener(buttonClicked);
+        opt2.setOnClickListener(buttonClicked);
+        opt3.setOnClickListener(buttonClicked);
+        opt4.setOnClickListener(buttonClicked);
+        
+        new BackgroundWorker().execute("rock");
     }
-
-	@Override
-	public void urlReady(String url) {
-		player.stop();
-		player.play(this, Uri.parse(url), false, AudioManager.STREAM_MUSIC);
-		isDownloading = false;
+    
+    private android.widget.Button.OnClickListener buttonClicked = new android.widget.Button.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Button b = (Button)v;
+			if (b.getText().equals(correct)) {
+				Toast.makeText(Main.this, "CORRECT!", Toast.LENGTH_SHORT).show();
+			}
+			else {
+				Toast.makeText(Main.this, "WRONGO!", Toast.LENGTH_SHORT).show();
+			}
+			new BackgroundWorker().execute("rock");
+		}};
+    
+	public void sampleRetrievalError() {
+		runOnUiThread(new Runnable() { public void run() {Toast.makeText(Main.this, "error getting song sample!  Retrying.", Toast.LENGTH_SHORT).show();}});
+		new BackgroundWorker().execute("rock");
 	}
 	
-	@Override
-	public void somethingWentWrong() {
-		Toast.makeText(this, "error getting song sample!", Toast.LENGTH_SHORT).show();
-		isDownloading = false;
+	String trackToString(final Track track) {
+		return track.getArtist() + " - " + track.getName();
+	}
+	
+	public void displayOptions(final List<Track> tracks) {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				if (tracks.size() < 4) {
+					Log.d("displayOptions", "DID NOT GET ENOUGH TRACKS");
+					sampleRetrievalError();
+					return;
+				}
+				Track rightTrack = tracks.remove(0);
+				correct = trackToString(rightTrack);
+				Collections.shuffle(tracks);
+				tracks.add((int)(Math.random() * 4), rightTrack);
+
+				opt1.setText(trackToString(tracks.get(0)));
+				opt2.setText(trackToString(tracks.get(1)));
+				opt3.setText(trackToString(tracks.get(2)));
+				opt4.setText(trackToString(tracks.get(3)));
+			}});
+	}
+	
+	class BackgroundWorker extends UserTask<String, List<Track>, List<Track>> {
+		
+		private LastFMDataProvider lastfm = new LastFMDataProvider();
+		private PreviewURLProvider itunes = new PreviewURLProvider();
+		
+		@Override
+		public List<Track> doInBackground(String... tags) {
+			Track correct = lastfm.getRandomTopTrack(1000);
+		//	Track correct = lastfm.getRandomTrackForTag(tags[0], 1000);
+			Collection<Track> alternatives = lastfm.getSimilarTracks(correct);
+			if (alternatives == null)
+				sampleRetrievalError();
+			final List<Track> tracks = new LinkedList<Track>(alternatives);
+			itunes.getPreviewURL(trackToString(correct), new PreviewURLListener() {
+				@Override
+				public void somethingWentWrong() {
+					sampleRetrievalError();
+				}
+
+				@Override
+				public void urlReady(String url) {
+					
+					player.stop();
+					// I should be able to reuse the same MediaPlayer, but I keep
+					// getting errors that I don't yet understand about invalid states
+					player.release();
+					player = new MediaPlayer();
+					try {
+						player.setDataSource(url);
+						player.prepare();
+						player.start();
+						if (tracks.size() < 4)
+							sampleRetrievalError();
+						else
+							displayOptions(tracks);
+					} catch (Exception e) {
+						sampleRetrievalError();
+						e.printStackTrace();
+						return;
+					}
+				}});
+			tracks.add(0, correct);
+			return tracks;
+		}
 	}
 }
