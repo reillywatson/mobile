f6d02866469bcf1a8427bcde1d1ea4fd708394f5
reilly@reilly-mbpro <reilly@reilly-mbpro>
2010-11-18 01:41:19 -0500

    Remove duplicates code actually removes duplicates now, instead of just mentioning them

--- incubator/quotedb/removeduplicates.py
+++ incubator/quotedb/removeduplicates.py
@@ -1,16 +1,44 @@
 import difflib
 import BeautifulSoup
+import sys
 
 def main():
-	file = open('quotes1.txt').read()
+	sys.stderr.write('reading file\n')
+	file = open('thinkexist.txt').read()
+	sys.stderr.write('beautiful souping\n')
 	soup = BeautifulSoup.BeautifulStoneSoup(file)
-	quotes = [str(x.text) for x in soup.findAll('text')]
-	suspiciousQuotes = []
+	sys.stderr.write('splitting\n')
+	quotes = soup.findAll('quote')
+	quoteTexts = [x.textTag.text for x in quotes]
+	authors = set([x.text for x in soup.findAll('author')])
+	quotesByAuthor = {}
 	for quote in quotes:
-		matches = difflib.get_close_matches(quote, quotes, 3, 0.8)
-		if len(matches) > 1:
-			print 'hey these are awfully similar:'
-			print matches
+		author = quote.author.text
+		if not quotesByAuthor.has_key(author):
+			quotesByAuthor[author] = []
+		quotesByAuthor[author].append(quote)
+	authorNo = 0
+	numAuthors = len(authors)
+	for author in authors:
+		authorNo = authorNo + 1
+		try:
+			sys.stderr.write(str(authorNo) + ' / ' + str(numAuthors) + ' - ' + author + '(' + str(len(quotesByAuthor[author])) + ')\n')
+		except:
+			pass
+		quoteTextsByAuthor = [x.textTag.text for x in quotesByAuthor[author]]
+		for quote in quoteTextsByAuthor:
+			matches = difflib.get_close_matches(quote, quoteTextsByAuthor, 3, 0.8)
+			if len(matches) > 1:
+				try:
+					sys.stderr.write('found match')
+					sys.stderr.write(matches[0])
+					sys.stderr.write(matches[1] + '\n')
+				except:
+					pass
+				matchingTags = [x for x in quotesByAuthor[author] if x.textTag.text in matches]
+				for i in range(1, len(matchingTags)):
+					matchingTags[i].extract()
+	print soup
 
 if __name__ == '__main__':
 	main()
