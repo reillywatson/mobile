e82a49047fc657475f4111f05258cea721a5c805
Reilly Watson <reillywatson@gmail.com>
2012-08-13 21:24:27 -0400

    Empty project

---
+++ iphone/OfflineWikipedia/Offline Wikipedia/Offline WikipediaTests/Offline_WikipediaTests.m
@@ -0,0 +1,32 @@
+//
+//  Offline_WikipediaTests.m
+//  Offline WikipediaTests
+//
+//  Created by Reilly Watson on 2012-08-13.
+//  Copyright (c) 2012 Reilly Watson. All rights reserved.
+//
+
+#import "Offline_WikipediaTests.h"
+
+@implementation Offline_WikipediaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Offline WikipediaTests");
+}
+
+@end
